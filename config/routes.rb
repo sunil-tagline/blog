786cfd0a87f17025e532fca59373e276eebb343d
{sunil-tagline} <{sunils.tagline@gmail.com}>
2023-07-07 16:56:10 +0530

    Comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
+  resources :articles do
+    resources :comments
+  end
 end
